5a032b08dbbeadb475b261ea58d7bd6aeaa7ebe4
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-07-19 14:47:53 +0200

    Create Tag and Blog models

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    full_image = models.ImageField(upload_to='full_images/')
+    thumb_image = models.ImageField(upload_to='thumb_images/', blank=True, null=True)
+    tags = models.ManyToManyField(Tag, related_name='blogs')
+    created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        # Add logic here to automatically create thumb_image from full_image
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
