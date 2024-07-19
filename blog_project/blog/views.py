7c1f5960570aec39b308f784d6cc34a8e6e941f8
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-07-19 14:50:31 +0200

    Create homepage view

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Blog, Tag
 
-# Create your views here.
+
+def homepage(request):
+    featured_blogs = Blog.objects.all().order_by("-created_at")[:3]
+    tags = Tag.objects.all()
+
+    return render(
+        request, "blog/homepage.html", {"featured_blogs": featured_blogs, "tags": tags}
+    )
+
