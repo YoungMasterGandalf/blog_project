83b6178f4c46c1577e1bf5564cda879bfcaa1a3c
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-07-20 20:33:44 +0200

    Register models in admin

--- blog_project/blog/admin.py
+++ blog_project/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Blog, Tag
 
-# Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    search_fields = ('title', 'content')
+    list_filter = ('tags',)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
