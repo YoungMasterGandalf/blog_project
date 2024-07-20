5da8e18dd47dbf2e5f09ed6909577fc3769b7577
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-07-20 21:59:39 +0200

    Introduce search view

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Blog, Tag
 from django.core.paginator import Paginator
+from django.db.models import Q
+
+from .models import Blog, Tag
+from .forms import SearchForm
 
 
 def homepage(request):
@@ -21,6 +24,7 @@ def blog_list(request):
 
     return render(request, "blog/blog_list.html", context)
 
+
 def blog_tag(request, tag):
     blogs = Blog.objects.filter(tags__name__icontains=tag).order_by("-created_at")
     paginator = Paginator(blogs, 10)
@@ -37,3 +41,20 @@ def blog_detail(request, slug):
 
     return render(request, "blog/blog_detail.html", context)
 
+
+def search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Blog.objects.filter(
+                Q(title__icontains=query) | Q(tags__name__icontains=query)
+            ).distinct()
+
+    context = {"form": form, "query": query, "results": results}
+
+    return render(request, "blog/search.html", context)
