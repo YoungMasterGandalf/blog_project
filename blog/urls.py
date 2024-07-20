a524cfa324ad5cd4a3e048245416ad4eabd8d779
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-07-20 22:06:19 +0200

    Restructure repo --> get rid of nested blog_project dir

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('blog/', views.blog_list, name='blog_list'),
-    path('tags/', views.tag_list, name='tag_list'),
     path('blog/<slug:slug>/', views.blog_detail, name='blog_detail'),
     path("tag/<tag>/", views.blog_tag, name="blog_tag"),
     path('search/', views.search, name='search'),
